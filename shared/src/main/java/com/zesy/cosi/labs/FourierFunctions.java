d0ac0896d8683640f721f02a36d5ffaeb357c183
zoxal <zzoxall@gmail.com>
2018-02-13 13:45:55 +0300

    Fourier stab and junit dependency

---
+++ shared/src/main/java/com/zesy/cosi/labs/FourierFunctions.java
@@ -0,0 +1,17 @@
+package com.zesy.cosi.labs;
+
+import java.util.function.Function;
+
+public class FourierFunctions {
+    public static Object[] getDiscreteFourierTransform(Function<Float, Float> function, float period, int N) {
+        return null;
+    }
+
+    public static Object[] getFastFourierTransform(Function<Float, Float> function, float period, int N) {
+        return null;
+    }
+
+    public static Object[] getReverseFourierTransform(Object[] values, float period, int N) {
+        return null;
+    }
+}
